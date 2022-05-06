082b8819212b5660945fdfd193f61fb0664f7aa9
NicholasDelianedis <nicdelianedis@yahoo.com>
2022-05-05 17:50:14 -0700

    Create ScraperAnalysis

--- IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
+++ IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
@@ -2,6 +2,7 @@
 # scrapy crawl imdb_spider -o movies.csv
 
 import scrapy
+from scrapy.http import Request
 
 class ImdbSpider(scrapy.Spider):
     name = 'imdb_spider'
@@ -9,20 +10,19 @@ class ImdbSpider(scrapy.Spider):
     start_urls = ['https://www.imdb.com/title/tt0898266/']
 
     def parse(self, response):
-        filename = f'imdb.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-
- #   def parse(self, response):
- #       new_url = response.url + "fullcredits"
- #       request = Request(new_url, callback = parse_full_credits)
- #       yield request
+        new_url = response.url + "fullcredits"
+        yield Request(new_url, callback = self.parse_full_credits)
     
     def parse_full_credits(self, response):
-        [a.attrib["href"] for a in response.css("td.primary_photo a")]
-        Request(new_url, callback = parse_actor_page)
-
-    def parse_actor_page(self, repsonse):
-        element.css("::attr(id)")
-        element.css("div.filmo-row")
-        element.css("a::text")
+        for i in [a.attrib["href"] for a in response.css("td.primary_photo a")]:
+            new_url = response.urljoin(i)
+            yield Request(new_url, callback = self.parse_actor_page)
+            
+    def parse_actor_page(self, response):
+        actor = response.css("div.name-overview-widget h1.header span.itemprop::text").get()
+        for element in response.css("div.filmo-row"):
+            movietv = element.css("div.filmo-row b a::text").get()
+            yield {
+                "actor" : actor, 
+                "movie_or_TV_name" : movietv
+            }
---
+++ ScrapeAnalysis.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+results = pd.read_csv('results.csv')
+
+results["movie"] = results["movie_or_TV_name"]
+results.head()
+
+resultcount = pd.DataFrame({'movie':results.groupby("movie").size().index, 'number of shared actors':results.groupby("movie").size().values})
+top10 = resultcount.sort_values(["number of shared actors"], ascending = False)
+top10[:10]
+
+import plotly.express as px
+fig = px.bar(top10[:100], x='movie', y='number of shared actors')
+fig.show()
