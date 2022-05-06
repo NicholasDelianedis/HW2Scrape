7cba4624a798280320f2f89ec0e43daed60be8a0
NicholasDelianedis <nicdelianedis@yahoo.com>
2022-05-05 17:38:05 -0700

    Changes

--- IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
+++ IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
@@ -1,4 +1,4 @@
-# to run 
+# to run GITHUB
 # scrapy crawl imdb_spider -o movies.csv
 
 import scrapy
@@ -9,9 +9,20 @@ class ImdbSpider(scrapy.Spider):
     start_urls = ['https://www.imdb.com/title/tt0898266/']
 
     def parse(self, response):
-        new_url = response.url + "fullcredits"
-        request = Request(new_url, callback = parse_full_credits)
-        yield request
+        filename = f'imdb.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+
+ #   def parse(self, response):
+ #       new_url = response.url + "fullcredits"
+ #       request = Request(new_url, callback = parse_full_credits)
+ #       yield request
     
     def parse_full_credits(self, response):
         [a.attrib["href"] for a in response.css("td.primary_photo a")]
+        Request(new_url, callback = parse_actor_page)
+
+    def parse_actor_page(self, repsonse):
+        element.css("::attr(id)")
+        element.css("div.filmo-row")
+        element.css("a::text")
