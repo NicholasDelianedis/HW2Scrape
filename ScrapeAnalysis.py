7f1e815f40754f0e3e9bd6be6c5f9165ab090d38
NicholasDelianedis <nicdelianedis@yahoo.com>
2022-05-11 12:54:35 -0700

    Update ScrapeAnalysis.py

--- ScrapeAnalysis.py
+++ ScrapeAnalysis.py
@@ -1,9 +1,7 @@
 import pandas as pd
 
 results = pd.read_csv('results.csv')
-
 results["movie"] = results["movie_or_TV_name"]
-results.head()
 
 resultcount = pd.DataFrame({'movie':results.groupby("movie").size().index, 'number of shared actors':results.groupby("movie").size().values})
 top10 = resultcount.sort_values(["number of shared actors"], ascending = False)
